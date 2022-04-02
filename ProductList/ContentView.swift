a927abd139f44735f8b16ce9acc8903f8697bbc7
Villory <villoryhoney@gmail.com>
2022-04-02 23:27:14 +0300

    Initial Commit

---
+++ ProductList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProductList
+//
+//  Created by Vitaliy Petrovskiy on 2.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProductList/ProductListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListApp.swift
+//  ProductList
+//
+//  Created by Vitaliy Petrovskiy on 2.04.22.
+//
+
+import SwiftUI
+
+@main
+struct ProductListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
